d423fcf79b3c972ce4f2bb3762219a3b036d9875
Rafael <122923722+Bowwaboumba@users.noreply.github.com>
2023-06-10 22:39:54 +0200

    Add files via upload
    Fahrenheit to Celcius ; Celcius to 
    Fahrenheit !

---
+++ C to F.py
@@ -0,0 +1,8 @@
+def convert(s):
+  c = float(s)
+  f = 9/5 * c + 32
+  return f
+
+print(convert(input()))
+
+input()
---
+++ F to C.py
@@ -0,0 +1,8 @@
+def convert(s):
+  c = float(s)
+  f = (c - 32) * 5 / 9
+  return f
+
+print(convert(input()))
+
+input()
